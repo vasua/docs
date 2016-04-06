7916562913e81e54e4ed191b8337242e79f02918
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-04-06 21:09:12 +0200

    Site updated at 2016-04-06 19:09:12 UTC

--- javascripts/lang-manager.js
+++ javascripts/lang-manager.js
@@ -9,7 +9,7 @@ $(function() {
     "javascript": "Javascript","java": "Java","python": "Python","php": "PHP","ruby": "Ruby","nodejs": "Node.js","objc": "Obj-C","swift": "Swift","go": "Swift","csharp": "C# .Net","cplusplus": "C++","c": "C","appcelerator": "Appcelerator","phonegap": "PhoneGap"
   };
   var languageExtensions = {
-    "javascript": "js","java": "java","python": "java","php": "php","ruby": "ruby","nodejs": "js","objc": "m","swift": "swift","go": "go","csharp": "cs","cplusplus": "cpp","c": "c","appcelerator": "js","phonegap": "js","html": "html","json": "json","sh": "sh","yaml": "yaml"
+    "javascript": "js","java": "java","python": "py","php": "php","ruby": "ruby","nodejs": "js","objc": "m","swift": "swift","go": "go","csharp": "cs","cplusplus": "cpp","c": "c","appcelerator": "js","phonegap": "js","html": "html","json": "json","sh": "sh","yaml": "yaml"
   };
 
   function friendlyLanguageFromId(languageId) {
